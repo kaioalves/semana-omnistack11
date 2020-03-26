351cdaf9524442fdf6bc2471e28b0c68c8f250ba
Kaio Alves <kaio.alves7@hotmail.com>
2020-03-26 11:34:18 -0300

    Criação IncidentController
    Métodos GET e POST

---
+++ backend/src/controllers/IncidentController.js
@@ -0,0 +1,26 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async index(request, response) {
+        const incidents = await connection('incidents').select('*');
+
+        return response.json(incidents);
+
+    },
+
+    async create(request, response) {
+        const { title, description, value } = request.body;
+
+        //ong_id = headers: authorization 4d9ff000
+        const ong_id = request.headers.authorization;
+
+        const [id] = await connection('incidents').insert({
+            title,
+            description,
+            value,
+            ong_id,
+        });
+
+        return response.json({ id });
+    }
+};
