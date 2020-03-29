89a4994be873c24e9e220d7b6e56334e66024a19
Kaio Alves <kaio.alves7@hotmail.com>
2020-03-29 00:35:13 -0300

    Criação da tela de Registro

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
@@ -10,7 +11,7 @@ export default function Logon() {
     return (
         <div className="logon-container">
             <section className="form">
-                <img src={logoImg} alt="Be the Hero" />
+                <img src={logoImg} alt="Be The Hero" />
 
                 <form>
                     <h1>Faça seu logon</h1>
@@ -18,10 +19,10 @@ export default function Logon() {
                     <input placeholder="Sua ID" />
                     <button className="button" type="submit">Entrar</button>
 
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041" />
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
             <img src={heroesImg} alt="Heroes" />
