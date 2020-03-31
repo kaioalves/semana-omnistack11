d0c83fac920fae480b774ccbc7405a152ff25b38
Kaio Alves <kaio.alves7@hotmail.com>
2020-03-31 12:29:00 -0300

    Deletando um Caso e atualizando a lista

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -24,6 +24,21 @@ export default function Profile() {
         })
     }, [ongId]);
 
+    async function handleDeleteIncident(id) {
+        try {
+            await api.delete(`incidents/${id}`, {
+                headers: {
+                    Authorization: ongId,
+                }
+            });
+
+            setIncidents(incidents.filter(incident => incident.id !== id));
+
+        } catch (err) {
+            alert('Erro ao deletar caso, tente novamente.')
+        }
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -50,7 +65,7 @@ export default function Profile() {
                         <strong>VALOR:</strong>
                         <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
 
-                        <button type="button">
+                        <button onClick={() => handleDeleteIncident(incident.id)} type="button">
                             <FiTrash2 size={20} color="#a8a8b3" />
                         </button>
                     </li>
